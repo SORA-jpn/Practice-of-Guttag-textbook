489325c91244acd85c29220bdd9c777e7f1eca52
SORA-jpn <ra.kutembeluku100029@gmail.com>
2026-03-26 17:53:00 +0000

    Add chap2.py

--- chap2.py
+++ chap2.py
@@ -3,16 +3,12 @@
 x, y, z = map(int, input("Enter 3 ints: ").split(","))
 
 answer = min(x, y, z)
-
 if x % 2 != 0:
     answer = x
-
 if y % 2 != 0 and y > answer:
     answer = y
-
 if z % 2 != 0 and z > answer:
     answer = z
-
 print(answer)
 
 
