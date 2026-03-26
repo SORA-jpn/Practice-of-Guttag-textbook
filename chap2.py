caf0add0dd8c7ddd504f2547be20b2df84364eea
SORA-jpn <ra.kutembeluku100029@gmail.com>
2026-03-26 21:19:07 +0000

    Add chap2

--- chap2.py
+++ chap2.py
@@ -1,6 +1,6 @@
 # P.20 指練習(教科書に解答例あり)
 
-x, y, z = map(int, input("Enter 3 ints: ").split(","))
+x, y, z = map(int, input("Enter 3 positive integers: ").split(","))
 
 answer = min(x, y, z)
 if x % 2 != 0:
@@ -17,5 +17,20 @@ print(answer)
 birthday = input('Enter your birthday(mm/dd/yyyy)')
 month, day, year = birthday.split('/')
 print(f'You were born in the year {year}.')
+
+
+
+#P.29 指練習
+num_x = int(input('How many times should I print the letter X? '))
+to_print = ''
+
+# ここからがコメント部
+while num_x > 0:
+    to_print += 'X'
+    num_x -= 1
+    
+print(to_print)
+
+
 
 #
