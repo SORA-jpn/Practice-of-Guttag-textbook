050bdb702f68c4e6589560c4e852e1669eca4710
SORA-jpn <ra.kutembeluku100029@gmail.com>
2026-03-26 21:28:32 +0000

    Add chap2

--- chap2.py
+++ chap2.py
@@ -1,7 +1,5 @@
 # P.20 指練習(教科書に解答例あり)
-
 x, y, z = map(int, input("Enter 3 positive integers: ").split(","))
-
 answer = min(x, y, z)
 if x % 2 != 0:
     answer = x
@@ -20,17 +18,27 @@ print(f'You were born in the year {year}.')
 
 
 
-#P.29 指練習
+# P.29 指練習
 num_x = int(input('How many times should I print the letter X? '))
 to_print = ''
-
 # ここからがコメント部
 while num_x > 0:
     to_print += 'X'
     num_x -= 1
-    
 print(to_print)
 
 
 
-#
+# P.30 指練習(本来はwhile文で解くことを意図された問題だが、for文による解法の方が楽？)
+nums = list(map(int, input("Enter 10 positive integers: ").split()))
+max_odd = None
+
+for n in nums:
+    if n % 2 != 0:  
+        if max_odd is None or n > max_odd:
+            max_odd = n
+
+if max_odd is None:
+    print("There are no odd numbers.")
+else:
+    print(f"The largest odd number is {max_odd}.")
